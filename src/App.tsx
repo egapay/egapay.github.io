f6465045f261264ccb000faf1fc14bec74eee95a
ethangapay <ethantgapay@gmail.com>
2026-06-21 16:30:57 -0400

    fixed page scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Home from "./pages";
 function App() {
 
   return (
-    <div className="sm:min-h-screen md:h-screen overflow-y-auto flex flex-col">
+    <div className="min-h-screen overflow-y-auto flex flex-col">
       {/* bg-gradient-to-br from-slate-900 via-slate-800 to-slate-900 */}
       <Home />
     </div>
